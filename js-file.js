69fe6205d9977db5e20284934f6e628ab479735a
Jasraj Aulakh <177463743+Jasraj02@users.noreply.github.com>
2024-08-26 02:19:09 +0100

    Add darkening feature
    Made the squares darken by 10% upon each mouse
    enter. This involved making the eventListener
    addition into a function so that it could be
    added upon each time the grid is changed.

--- js-file.js
+++ js-file.js
@@ -34,15 +34,42 @@ function removeGrid () {
 }
 
 
-createGrid(defaultSize)
-const allDiv = document.querySelectorAll("div")
-
 const gridButton = document.querySelector("button")
-
 gridButton.addEventListener("click", () => {
-    const gridNumber = prompt("Enter the grid size: ")
-    gridNumber = Number(gridNumber)
-    removeGrid()
-    createGrid(gridNumber)
-})
+    let gridNumber = prompt("Enter the grid size: ");
+    gridNumber = parseInt(gridNumber);
+
+    if (!isNaN(gridNumber)) { 
+        removeGrid();
+        createGrid(gridNumber);
+        allLowerDiv = document.querySelectorAll("div>div")
+        opacityAdder()
+    } else {
+        alert("Please enter a valid number!");
+    }
+});
+
+
+createGrid(defaultSize)
+var allLowerDiv = document.querySelectorAll("div>div")
+opacityAdder()
+
+function opacityAdder() {
+    allLowerDiv.forEach( (item) => {
+        item.addEventListener("mouseenter", () => {
+            let currentOpacity = parseFloat(item.style.opacity) || 0; 
+            currentOpacity += 0.1;
+            if (currentOpacity > 1) { currentOpacity = 1; }
+            item.style.opacity = currentOpacity;
+            if (!item.classList.contains("etch")) {
+                item.classList.add("etch"); }
+    });
+});
+
+}
+
+
+
+
+
 
